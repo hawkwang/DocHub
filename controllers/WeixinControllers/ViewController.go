d997af7002017d0acb646bff34a8903cf61aebb4
hawkwang <oncwnuI0GjHx8oZXI_2ahxxa1QcU@git.weixin.qq.com>
2020-03-07 21:30:25 +0800

    add restful endpoint for weixin

--- controllers/WeixinControllers/ViewController.go
+++ controllers/WeixinControllers/ViewController.go
@@ -7,6 +7,7 @@ import (
 
 	"time"
 
+	"github.com/hawkwang/DocHub/controllers/HomeControllers"
 	"github.com/hawkwang/DocHub/helper"
 	"github.com/hawkwang/DocHub/models"
 	"github.com/astaxie/beego"
